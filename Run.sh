799ac5a5087bea0a6fdd360fc7d0ed5f7ed92dfe
Mingzhi Lin <mingzhi9@gmail.com>
2017-11-06 15:39:26 -0500

    update anthina example

--- Run.sh
+++ Run.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-mcorr --progress anthina_core.xmfa anthina_core.csv
-python $GOPATH/src/github.com/mingzhi/mcorr/cmd/fitting/FitP2.py anthina_core.csv anthina_core
+mcorr-xmfa anthina_core.xmfa anthina_core
+python $HOME/go/src/github.com/kussell-lab/mcorr/cmd/fitting/FitP.py anthina_core.csv anthina_core
